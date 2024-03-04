03369645b2d30a5f506227c1f6b765457ae614aa
reactjs44 <160457495+reactjs44@users.noreply.github.com>
2024-03-04 11:51:00 +0000

    all commit

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
 import express from "express";
 const app = express();
 import db from "./db.js";
-import bodyParser from 'body-parser'
-import Person from './models/person.js'
+import bodyParser from "body-parser";
+import Person from "./models/person.js";
 
-import personRoutes from './routes/personRoutes.js'
-import passport from 'passport';
+import personRoutes from "./routes/personRoutes.js";
+import passport from "passport";
 
-import passportLocal from 'passport-local';
-
-
+import passportLocal from "passport-local";
 
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
-passport.use(new passportLocal.Strategy(async (username, password, done) => {
+passport.use(
+  new passportLocal.Strategy(async (username, password, done) => {
     try {
-        const person = await Person.findOne({ username: username });
+      const person = await Person.findOne({ username: username });
 
-        if (!person) {
-            return done(null, false, { message: "Invalid username" });
-        }
+      if (!person) {
+        return done(null, false, { message: "Invalid username" });
+      }
 
-        const passwordMatch = await person.comparePassword(password);
+      const passwordMatch = await person.comparePassword(password);
 
-        if (passwordMatch) {
-            return done(null, person);
-        } else {
-            return done(null, false, { message: "Invalid password" });
-        }
-
+      if (passwordMatch) {
+        return done(null, person);
+      } else {
+        return done(null, false, { message: "Invalid password" });
+      }
     } catch (error) {
-        return done(error);
+      return done(error);
     }
-}));
+  }),
+);
 
-
- const middleWare=passport.authenticate('local',{session:false});
-app.get("/",middleWare, (req, res) => {
+const middleWare = passport.authenticate("local", { session: false });
+app.get("/", middleWare, (req, res) => {
   res.send("server is running");
 });
 
-app.use('/person',middleWare,personRoutes);
+app.use("/person", middleWare, personRoutes);
 
 app.listen(3000, () => {
   console.log("Express server initialized");
