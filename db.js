58467761bf880ee04263b4facc528f6c61de88d5
reactjs44 <160457495+reactjs44@users.noreply.github.com>
2024-03-04 18:43:39 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose'
 
 
- const mongodbURL='mongodb+srv://leariningcourses:0OH5tRklU0YHcIc6@cluster0.70nycvo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-
+ 
 mongoose.connect(mongodbURL);
 
 
